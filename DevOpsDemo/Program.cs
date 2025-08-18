2bb4d0bd521109e00a2a1dd3e4be4bf4b249368d
Josef Procházka <josef.prochazka@bcpraha.com>
2025-08-18 22:24:59 +0200

    some comments

--- DevOpsDemo/Program.cs
+++ DevOpsDemo/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,9 +22,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-// test change
-
-// trigger CI
 
 public partial class Program { }
--- WeatherEndpointTests2/UnitTest1.cs
+++ WeatherEndpointTests2/UnitTest1.cs
@@ -1,17 +1,27 @@
+// Tento testovací soubor ovìøuje funkènost endpointu /weatherforecast ve webové aplikaci.
+// Používá xUnit a tøídu WebApplicationFactory k vytvoøení testovací instance aplikace,
+// díky èemuž není potøeba spouštìt aplikaci "naživo". 
+// 
+// Tøída WeatherEndpointTests2 implementuje IClassFixture<WebApplicationFactory<Program>>,
+// což znamená, že pro každý test se vytvoøí HttpClient napojený na testovací server.
+// 
+// Konkrétní test Get_WeatherForecast_ReturnsOk odešle GET požadavek na /weatherforecast
+// a oèekává odpovìï se stavovým kódem 200 OK. 
+// Tím ovìøuje, že endpoint funguje a aplikace vrací validní odpovìï.
+//
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
-using Xunit;
 
 public class WeatherEndpointTests2 : IClassFixture<WebApplicationFactory<Program>>
 {
-    private readonly HttpClient _client;
-    public WeatherEndpointTests2(WebApplicationFactory<Program> factory)
-        => _client = factory.CreateClient();
+	private readonly HttpClient _client;
+	public WeatherEndpointTests2(WebApplicationFactory<Program> factory)
+		=> _client = factory.CreateClient();
 
-    [Fact]
-    public async Task Get_WeatherForecast_ReturnsOk()
-    {
-        var resp = await _client.GetAsync("/weatherforecast");
-        Assert.Equal(HttpStatusCode.OK, resp.StatusCode);
-    }
+	[Fact]
+	public async Task Get_WeatherForecast_ReturnsOk()
+	{
+		var resp = await _client.GetAsync("/weatherforecast");
+		Assert.Equal(HttpStatusCode.OK, resp.StatusCode);
+	}
 }
